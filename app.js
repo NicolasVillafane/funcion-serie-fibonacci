672ce6f1f9af433b4ded310cf58507f96f1db4c1
Nicolas <nivi1023@gmail.com>
2021-04-22 12:41:26 -0300

    ahora los numeros estan dentro de un array

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const fibonacci = (num) => {
   let n1 = 0;
   let n2 = 1;
-  let nu;
+  let numeroSiguiente;
+  let arr = [];
   if (num <= 0) {
     console.log('Solo numeros mayores a 0');
   } else {
     for (let i = 1; i <= num; i++) {
-      console.log(n1);
-      nu = n1 + n2;
+      arr.push(n1);
+      numeroSiguiente = n1 + n2;
       n1 = n2;
-      n2 = nu;
+      n2 = numeroSiguiente;
     }
+    console.log(arr);
   }
 };
 
