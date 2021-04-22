784feb5db6f514e0ecdb586b6f64b607d01327e6
Nicolas <nivi1023@gmail.com>
2021-04-22 12:38:26 -0300

    añadiendo validacion

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const fibonacci = (num) => {
   let n1 = 0;
   let n2 = 1;
   let nu;
-  for (let i = 1; i <= num; i++) {
-    console.log(n1);
-    nu = n1 + n2;
-    n1 = n2;
-    n2 = nu;
+  if (num <= 0) {
+    console.log('Solo numeros mayores a 0');
+  } else {
+    for (let i = 1; i <= num; i++) {
+      console.log(n1);
+      nu = n1 + n2;
+      n1 = n2;
+      n2 = nu;
+    }
   }
 };
 
-fibonacci(7);
+fibonacci(10);
